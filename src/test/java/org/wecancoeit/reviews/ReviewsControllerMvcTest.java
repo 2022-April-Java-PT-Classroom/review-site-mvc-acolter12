a73a0b0d8ac797f9a684188ce91e1379be8c3b26
acolter12 <acolter@kent.edu>
2022-06-09 16:42:20 -0400

    ALL test currently passing, had to add mockito-core 3.7.7 dependency.

--- src/test/java/org/wecancoeit/reviews/ReviewsControllerMvcTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewsControllerMvcTest.java
@@ -6,6 +6,7 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -35,7 +36,7 @@ public class ReviewsControllerMvcTest {
 
     @Test
     public void shouldBeOkForAllCoursesInTheTemplate() throws Exception {
-        mockMvc.perform(get("/reviews")).andExpect(status().isOk()).andExpect(view().name("reviewsTemplate"));
+        mockMvc.perform(get("/reviews")).andExpect(status().isOk()).andExpect(view().name("reviewsTemplate.html"));
     }
 
     @Test
